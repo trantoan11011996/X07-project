17218b091a84583f2e64b2ca1a9feb2fce94eb59
toan <trantoan11011996@gmail.com>
2022-11-22 20:18:18 +0700

    first commit

---
+++ final-project/src/Components/MainApp/MainApp.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { AuthContext } from "../../Context/Context";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "../Static/Header/Header";
+import Footer from "../Static/Footer/Footer";
+import HomePage from "../HomePage/HomePage";
+
+export default function MainApp() {
+
+
+
+  return (
+    <AuthContext.Provider >
+      <BrowserRouter>
+        <div className="main-app">
+          <Header />
+          <Routes>
+                <Route path="/" element={<HomePage/>}></Route>
+          </Routes>
+          <Footer/>
+        </div>
+      </BrowserRouter>
+    </AuthContext.Provider>
+  );
+}
