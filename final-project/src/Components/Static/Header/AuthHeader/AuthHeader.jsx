b07286246beac1744e522946d162678e001d60b9
Quang Minh <nguyendoquangminh2112@gmail.com>
2022-11-26 12:07:56 +0700

    upload redux persits

--- final-project/src/Components/Static/Header/AuthHeader/AuthHeader.jsx
+++ final-project/src/Components/Static/Header/AuthHeader/AuthHeader.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Menu,Avatar } from "antd";
+import { Menu, Avatar } from "antd";
 import {
   UserOutlined,
   KeyOutlined,
@@ -23,55 +23,62 @@ const AuthHeader = ({ mode }) => {
         theme="white"
         mode={mode}
         onClick={handleMenuClick}
-        style={{ marginBottom: 15, boxShadow: "2px 5px 7px -1px rgba(184,165,184,1)" }}
+        style={{
+          marginBottom: 15,
+          boxShadow: "2px 5px 7px -1px rgba(184,165,184,1)",
+        }}
       >
-         <Menu.Item>
-              <img
-                src="logo192.png"
-                alt="logo"
-                style={{ marginLeft: 15, height: 35 }}
-              ></img>
-          </Menu.Item>
-          <div style={{ width: 1800 }}></div>
+        <Menu.Item>
+          <img
+            src="logo192.png"
+            alt="logo"
+            style={{ marginLeft: 15, height: 35 }}
+          ></img>
+        </Menu.Item>
+        <div style={{ width: 1800 }}></div>
         {isAuthenticated ? (
           <></>
         ) : (
           <>
-          <div style={{ width: 1600 }}></div>
+            <div style={{ width: 1600 }}></div>
             <Menu.Item key="/login">Login</Menu.Item>
             <Menu.Item key="/register">Register</Menu.Item>
-           
           </>
         )}
 
         {isAuthenticated && (
-           <>
-        <Menu theme="white" mode={mode} onClick={handleMenuClick} style={{}}>
-      <Menu.SubMenu
-        title={
           <>
-            <Avatar icon={<UserOutlined />} />
+            <Menu
+              theme="white"
+              mode={mode}
+              onClick={handleMenuClick}
+              style={{}}
+            >
+              <Menu.SubMenu
+                title={
+                  <>
+                    <Avatar icon={<UserOutlined />} />
+                  </>
+                }
+              >
+                <Menu.Item>
+                  <UserOutlined /> Profile
+                </Menu.Item>
+                <Menu.Item key="/update_password">
+                  <KeyOutlined /> Update Password
+                </Menu.Item>
+                <Menu.Item key={"/candidate"}>
+                  <FormOutlined /> Update Info cadidate
+                </Menu.Item>
+                <Menu.Item key={"/recruiter"}>
+                  <FormOutlined /> Update Info recruiter
+                </Menu.Item>
+                <Menu.Item key="/login">
+                  <LogoutOutlined /> Log out
+                </Menu.Item>
+              </Menu.SubMenu>
+            </Menu>
           </>
-        }
-      >
-        <Menu.Item>
-          <UserOutlined /> Profile
-        </Menu.Item>
-        <Menu.Item key="/update_password">
-            <KeyOutlined /> Update Password
-        </Menu.Item>
-        <Menu.Item key={"/candidate"}>
-          <FormOutlined /> Update Info cadidate
-        </Menu.Item>
-        <Menu.Item key={"/recruiter"}>
-          <FormOutlined /> Update Info recruiter
-        </Menu.Item>
-        <Menu.Item>
-          <LogoutOutlined /> Log out
-        </Menu.Item>
-      </Menu.SubMenu>
-    </Menu>
-           </>
         )}
       </Menu>
     </div>
--- final-project/src/index.js
+++ final-project/src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { DatePicker } from "antd";
+import { PersistGate } from "redux-persist/integration/react";
 import "antd/dist/antd.min.css";
 import { Provider } from "react-redux";
-import store from "./store";
+import store, { persistor } from "./store";
 import GlobalStyles from "./Components/GlobalStyles/GlobalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <GlobalStyles>
-      <App />
-    </GlobalStyles>
+    <PersistGate loading={null} persistor={persistor}>
+      <GlobalStyles>
+        <App />
+      </GlobalStyles>
+    </PersistGate>
   </Provider>
 );
