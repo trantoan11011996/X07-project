fdb776e446859c66603a86b8fe799aae2a74f584
daoxuantung <xuantung1610@gmail.com>
2022-11-28 00:00:28 +0700

    fix header lan 2

--- final-project/src/Components/Static/Header/AuthHeader/AuthHeader.jsx
+++ final-project/src/Components/Static/Header/AuthHeader/AuthHeader.jsx
@@ -5,6 +5,7 @@ import {
   KeyOutlined,
   FormOutlined,
   LogoutOutlined,
+  SearchOutlined 
 } from "@ant-design/icons";
 import { Link, useNavigate } from "react-router-dom";
 import InfoUserDropDown from "../InfoUserDropdown/InfoUserDropdown";
@@ -23,30 +24,32 @@ const AuthHeader = ({ mode }) => {
         theme="white"
         mode={mode}
         onClick={handleMenuClick}
-        style={{ marginBottom: 15, height: 70, boxShadow: "2px 5px 7px -1px rgba(184,165,184,1)" , fontSize:"25px" }}
+        style={{ marginBottom: 15, height: 60, boxShadow: "2px 5px 7px -1px rgba(184,165,184,1)" , fontSize:"18px" }}
       >
          <Menu.Item key={"/"}>
               <img
                 src="logo192.png"
                 alt="logo"
-                style={{ marginLeft: 15, height: 60, marginTop: 5 }}
+                style={{ marginLeft: 15, height: 50, marginTop: 5 }}
               ></img>
           </Menu.Item>
           <div style={{ width: 1800 }}></div>
+          <Menu.Item key="/company"  style={{marginTop: 9}} >Công ty</Menu.Item>
+          <Menu.Item key="/job&location"  style={{marginTop: 9}} >Ngành nghề/Địa điểm</Menu.Item>
         {isAuthenticated ? (
           <></>
         ) : (
           <>
           <div style={{ width: 1600 }}></div>
-            <Menu.Item key="/login" style={{marginTop: 12}} >Đăng nhập</Menu.Item>
-            <Menu.Item key="/register"  style={{marginTop: 12}} >Đăng ký</Menu.Item>
-           
+            <Menu.Item key="/login" style={{marginTop: 9}} >Đăng nhập</Menu.Item>
+            <Menu.Item key="/register"  style={{marginTop: 9}} >Đăng ký</Menu.Item>
           </>
         )}
 
         {isAuthenticated && (
            <>
         <Menu theme="white" mode={mode} onClick={handleMenuClick} style={{}}>
+          
       <Menu.SubMenu
         title={
           <>
