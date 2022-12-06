7bfd8cb7d9063f910a446d88dd913715bd8ef030
Thuan-VQ <qthuan1997@gmail.com>
2022-12-06 22:25:46 +0700

    job page

---
+++ final-project/src/Components/JobAndLocation/Jobs-Candidate/JobBanner.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export default function Jobbanner () {
+
+    return(
+        <Container>
+            
+        </Container>
+    )
+}
---
+++ final-project/src/Components/JobAndLocation/Jobs-Candidate/JobsDetailCandidate.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import JobItem from "./JobItem";
+
+export default function JobsDetailCandidate () {
+
+    return(
+        <Container>
+             
+             <JobItem />
+           
+        </Container>
+    )
+}
