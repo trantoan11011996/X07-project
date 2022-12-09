2831ad1652f1af5a57f7887d2b7cd1110764c467
daoxuantung <xuantung1610@gmail.com>
2022-12-09 13:06:49 +0700

    add css cho tin da dang, filter de sau

--- final-project/src/Components/AvailableRecruitment/JobItemRecruitment.jsx
+++ final-project/src/Components/AvailableRecruitment/JobItemRecruitment.jsx
@@ -1,57 +1,59 @@
-import React, { Fragment, useContext, useEffect } from "react";
+import React, { Fragment} from "react";
 import { AiOutlineHeart } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import styles from "./AvailableRecruitment.module.scss";
 import classNames from "classnames/bind";
-import { JobContext } from "../../Context/JobContext";
+
 const cx = classNames.bind(styles);
 
-function JobItemRecruitment  ({ myJobRecruitment }) {
-  
-  console.log('jobR', myJobRecruitment)
- 
+function JobItemRecruitment({ myJobRecruitment }) {
+  // console.log("jobR", myJobRecruitment);
+
   return (
     <Fragment>
-      {/* <li className={cx("list_group_item")}>
+      <li className={cx("list_group_item")}>
         <div className={cx("box_item")}>
           <div className={cx("images")}>
-            <img src={myJobRecruitment.name.info.avatar} alt="" />
+            <img src={myJobRecruitment.name.avatar} alt="" />
           </div>
           <div className={cx("content")}>
             <Link to="/" className={cx("title")}>
               {myJobRecruitment.title}
             </Link>
-            <Link to="/" className={cx("company")}>
-              {myJobRecruitment.name.info.name}
-            </Link>
+            <div>
+              <Link to="/" className={cx("company")}>
+                {myJobRecruitment.name.info.name}
+              </Link>
+            </div>
             <div className={cx("address")}>
-              {myJobRecruitment.name.location}
+              {myJobRecruitment.location.name}
             </div>
             <div className={cx("info_salary")}>
               <div className={cx("salary ")}>
-                {myJobRecruitment.salary}
+                <span> {myJobRecruitment.salary} </span>
               </div>
-              <div className={cx("category")}>
-              {myJobRecruitment.category.name}
             </div>
-            <div className={cx("deadline")}>
-              {myJobRecruitment.deadline}
+            <div className={cx("category")}>
+              <span>{myJobRecruitment.category.name}</span>
             </div>
-             <div className={cx("deadline")}>
-              {myJobRecruitment.createAt}
-            </div>
-              <div className={cx("save ")}>
-                {" "}
+            <div className={cx("small_detail")}>
+              <div className={cx("deadline")}>
+                Ngày tạo: {myJobRecruitment.deadline}
+              </div>
+              <div className={cx("deadline")}>
+                Ngày hết hạn: {myJobRecruitment.createAt}
+              </div>
+              <div className={cx("update")}>
                 <a href="/#">
                   <AiOutlineHeart />
-                  Lưu
+                  Cập Nhật
                 </a>
               </div>
             </div>
           </div>
         </div>
-      </li> */}
-       <li className={cx("list_group_item")}>
+      </li>
+      {/* <li className={cx("list_group_item")}>
       <div className={cx("box_item")}>
           <div className={cx("images")}>
             <img src={myJobRecruitment.name.info.avatar} alt="" />
@@ -92,9 +94,9 @@ function JobItemRecruitment  ({ myJobRecruitment }) {
             </div>
           </div>
         </div>
-        </li>
+        </li> */}
     </Fragment>
   );
-};
+}
 
 export default JobItemRecruitment;
