1b83a8c5ffbbd7983e3ea048b8f09354f9d45ebd
Thuan-VQ <qthuan1997@gmail.com>
2023-01-02 19:03:57 +0700

    adjust contact Item

--- final-project/src/Components/AboutPages/ContactPage/ContactItem.js
+++ final-project/src/Components/AboutPages/ContactPage/ContactItem.js
@@ -7,11 +7,11 @@ export default function ContactItem ({ data }) {
     return(
             <Card className="contact-card">
                 <Row className="mt-3">
-                    <Col sm={3} md={3} >
+                    <Col sm={4} md={4} >
                         <TfiHeadphoneAlt className="headPhone"></TfiHeadphoneAlt>
                     </Col>
 
-                    <Col sm={5} md={5}>
+                    <Col sm={6} md={6}>
                         <b> Nhân viên: {data.name}</b> <br />
                         <b> Sđt: {data.phoneNumber}</b>
                     </Col>
