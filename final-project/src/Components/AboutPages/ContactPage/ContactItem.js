bc3d3d2025be87f82511cb2ec35cde39f3137e04
toan <trantoan11011996@gmail.com>
2023-01-02 20:08:40 +0700

    update

--- final-project/src/Components/AboutPages/ContactPage/ContactItem.js
+++ final-project/src/Components/AboutPages/ContactPage/ContactItem.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
-import {TfiHeadphoneAlt} from "react-icons/tfi";
+import { TfiHeadphoneAlt } from "react-icons/tfi";
 
-export default function ContactItem ({ data }) {
+export default function ContactItem({ item, bgcolor,colorIcon }) {
+    useEffect(()=>{
+        window.scrollTo(0,0)
+    },[])
+  return (
+    <div className="contact-card">
+      <Row className="row-item-contact">
+        <Col sm={4} md={5} className="col-item-headphone">
+          <div
+            className="wrap-icon-contact"
+            style={{
+              backgroundColor: bgcolor,
+            }}
+          >
+            <TfiHeadphoneAlt
+              className="headPhone"
+              style={{
+                color: colorIcon,
+              }}
+            ></TfiHeadphoneAlt>
+          </div>
+          <div className="item-desc">
+            <b>Nhân viên: </b>
+            <b>Sđt: </b>
+          </div>
+        </Col>
 
-    return(
-            <Card className="contact-card">
-                <Row className="mt-3">
-                    <Col sm={4} md={4} >
-                        <TfiHeadphoneAlt className="headPhone"></TfiHeadphoneAlt>
-                    </Col>
-
-                    <Col sm={6} md={6}>
-                        <b> Nhân viên: {data.name}</b> <br />
-                        <b> Sđt: {data.phoneNumber}</b>
-                    </Col>
-                </Row>
-            </Card>
-    )
+        <Col sm={6} md={7}>
+          <p>{item.name}</p>
+          <p>{item.phoneNumber}</p>
+        </Col>
+      </Row>
+    </div>
+  );
 }
